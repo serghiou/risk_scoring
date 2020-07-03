26178186a16c5d4199d76bcfb5aab0c4853bd490
awilson12 <apfeifer@email.arizona.edu>
2020-07-02 18:21:21 -0700

    Updates based on new scenarios for figure 2

--- code for figures.R
+++ code for figures.R
@@ -106,26 +106,24 @@ ggarrange(A,B)
 #------------------------------------ figure 2 -------------------------------------------------------------
 
 
-subframe6<-frame[frame$risk.start==0.0505 & frame$days<=5 & frame$fraction.asymptomatic==0.25,]
-tempframe<-data.frame(risks=0.03,days=5.3,risk.start=0.0505,fraction.asymptomatic=0.25)
-subframe6<-rbind(subframe6,tempframe)
+subframe6<-frame[frame$risk.start==0.0505 & frame$days<=13 & frame$fraction.asymptomatic==0.25,]
 
-subframe7<-frame[frame$risk.start==0.0505 & frame$days<=7 & frame$fraction.asymptomatic==0.5,]
+subframe7<-frame[frame$risk.start==0.0505 & frame$days<=21 & frame$fraction.asymptomatic==0.5,]
 
 A.2<-ggplot(frame[frame$risk.start==0.0505,],aes(x=days,y=risks))+
   geom_point(aes(shape=as.character(fraction.asymptomatic),colour=as.character(fraction.asymptomatic)),size=4,alpha=0.8)+
-  geom_area(data=subframe2,aes(x = ifelse(days<=5.67 , days, 0)),fill="blue",alpha=0.3)+
-  geom_area(data=subframe6,aes(x = ifelse(days<=5.3 , days, 0)),fill="red",alpha=0.3)+
-  geom_area(data=subframe7,aes(x = ifelse(days<=7 , days, 0)),fill="grey",alpha=0.3)+
+  geom_area(data=subframe1,aes(x = ifelse(days<=9 , days, 0)),fill="blue",alpha=0.3)+
+  geom_area(data=subframe6,aes(x = ifelse(days<=13 , days, 0)),fill="red",alpha=0.3)+
+  geom_area(data=subframe7,aes(x = ifelse(days<=21 , days, 0)),fill="grey",alpha=0.3)+
   scale_shape_discrete(name="% Asymptomatic",labels=c("15%", "25%", "50%"))+
   scale_colour_discrete(name="% Asymptomatic",labels=c("15%", "25%", "50%"))+
   scale_x_continuous(name="Days Since Exposure")+
   scale_y_continuous(name="Risk of Infection",limits = c(0, 0.051))+
-  geom_hline(yintercept=0.03,linetype="dashed",size=1)+
-  annotate("text",x=19,y=0.041,label="3% Threshold",size=6.5)+
-  annotate("text",x=19,y=0.038,label="Quarantine for 5 days (15% Asymptomatic)",size=6.5)+
-  annotate("text",x=19,y=0.035,label="Quarantine for 6 days (25% Asymptomatic)",size=6.5)+
-  annotate("text",x=19,y=0.032,label="Quarantine for 7 days (50% Asymptomatic)",size=6.5)+
+  geom_hline(yintercept=0.01,linetype="dashed",size=1)+
+  annotate("text",x=18,y=0.036,label="1% Threshold",size=6.5)+
+  annotate("text",x=18,y=0.034,label="Quarantine for 10 days (15% Asymptomatic)",size=6.5)+
+  annotate("text",x=18,y=0.032,label="Quarantine for 14 days (25% Asymptomatic)",size=6.5)+
+  annotate("text",x=18,y=0.030,label="Quarantine for 22 days (50% Asymptomatic)",size=6.5)+
   theme_pubr()+theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20),
                      legend.text = element_text(size=20), legend.title=element_text(size=20),
                      axis.text=element_text(size=20),title=element_text(size=20))+
@@ -135,26 +133,40 @@ windows()
 A.2
 
 #negative test scenario
-days<-c(frame$days[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.15],figure2$Days)
-risks<-c(frame$risks[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.15],figure2$Risktest)
-type<-c(rep("No test",length(frame$days[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.15])),
+days<-c(frame$days[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.5],figure2$Days)
+risks<-c(frame$risks[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.5],figure2$Risktest)
+type<-c(rep("No test",length(frame$days[frame$risk.start==0.0505 & frame$fraction.asymptomatic==0.5])),
         rep("Negative Test Result",length(figure2$Days)))
 frame2<-data.frame(days=days,risks=risks,type=type)
-frame2<-frame2[frame2$days<=14,]
 
-subframe8<-frame2[frame2$type=="Negative Test Result" & frame2$days<=5,]
+subframe8<-frame2[frame2$type=="Negative Test Result" & frame2$days<=6,]
+#tempframe<-data.frame(days=4.5,risks=0.01,type="No Test")
+#subframe8<-rbind(subframe8,tempframe)
+subframe9<-frame2[frame2$type=="No test" & frame2$days<=21,]
 
-ggplot(data=frame2)+geom_point(aes(x=days,y=risks,group=type,colour=type,shape=type),size=3)+
-  geom_hline(yintercept=0.0241,linetype="dashed",size=1)+
-  geom_area(data=subframe2,aes(x = ifelse(days<=5.67 , days, 0),y=risks),fill="blue",alpha=0.3)+
-  #geom_area(data=subframe9,aes(x = ifelse(days<=5 , days, 0),y=risks),fill="red",alpha=0.3)+
-  theme_pubr()+theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20),
+
+windows()
+B.2<-ggplot(data=frame2)+geom_point(aes(x=days,y=risks,group=type,colour=type,shape=type),size=4)+
+    geom_hline(yintercept=0.01,linetype="dashed",size=1)+
+    geom_vline(xintercept=4,linetype="solid",size=1)+
+    geom_area(data=subframe8,aes(x = ifelse(days<=6 , days, 0),y=risks),fill="blue",alpha=0.3)+
+    geom_area(data=subframe9,aes(x = ifelse(days<=21 , days, 0),y=risks),fill="red",alpha=0.3)+
+    theme_pubr()+theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20),
                    legend.text = element_text(size=20), legend.title=element_text(size=20),
                    axis.text=element_text(size=20),title=element_text(size=20))+
-  scale_colour_discrete(name="")+
-  scale_shape_discrete(name="")+
-  scale_x_continuous(name="Days Since Exposure")+
-  scale_y_continuous(name="Risk of Infection",limits = c(0, 0.051))+
-  ggtitle("B. No Test vs. Negative Test Result")
+    annotate("text",x=18,y=0.036,label="1% Threshold",size=6.5)+
+    annotate("text",x=18,y=0.034,label="Quarantine for 7 days with Testing",size=6.5)+
+    annotate("text",x=18,y=0.032,label="Quarantine for 22 days without Testing",size=6.5)+
+   #annotate("text",x=14,y=0.048,label="Negative Test 4 Days Since Exposure",size=6.5)+
+    scale_colour_discrete(name="",labels=c("Negative Test Result on Day 4","No Test"))+
+    scale_shape_discrete(name="",labels=c("Negative Test Result on Day 4","No Test"))+
+    scale_x_continuous(name="Days Since Exposure")+
+    scale_y_continuous(name="Risk of Infection",limits = c(0, 0.052))+
+    ggtitle("B. No Test vs. Negative Test Result")
 
+
+windows()
+B.2
+
+ggarrange(A.2,B.2)
 
