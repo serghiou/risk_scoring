a6e0242dd5c5bb669882b08b89f652fc1d191089
awilson12 <apfeifer@email.arizona.edu>
2020-08-17 13:51:46 -0700

    updates to text sizes

--- evaluation of transmission risk levels_v2.R
+++ evaluation of transmission risk levels_v2.R
@@ -4,8 +4,8 @@ compare<-read.csv('compare.risk.level.csv')
 plotA<-ggplot(compare)+geom_bar(aes(x=Days.since.post.symptom.onset,y=Risk.value,fill=Risk.value),stat="identity")+
   scale_x_continuous(name="Days Relative to Symptom Onset")+scale_y_continuous("Transmission Risk Value")+
   theme_pubr()+
-  theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20),title = element_text(size=20),
-                           axis.text=element_text(size=20),legend.position = "none",legend.text=element_text(size=20))+
+  theme(axis.title.x = element_text(size=25),axis.title.y = element_text(size=25),title = element_text(size=25),
+                           axis.text=element_text(size=22),legend.position = "none",legend.text=element_text(size=25))+
   scale_fill_gradient(name="",low="lightgray",high="red")
 
 
